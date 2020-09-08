ce3df63151b37a45f1842e9be5e8570b841fc4ec
Tomás Migone <tomas@balena.io>
2020-09-08 14:29:14 -0300

    webserver: promisify code a bit. not tested.
    Change-type: patch
    Signed-off-by: Tomás Migone <tomas@balena.io>

--- webserver/server.js
+++ webserver/server.js
@@ -1,38 +1,33 @@
-const sqlite3 = require("sqlite3").verbose(); var express = require('express'); var app = express(); const {env} = require('process'); var fs = require('fs');
+const sqlite3 = require("sqlite3").verbose();
+const express = require('express');
+const app = express();
+const { env } = require('process');
+const fs = require('fs');
+const Minio = require('minio');
+const util = require('util');
+
+const dbAll = util.promisify(db.all)
+const dbRun = util.promisify(db.run)
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
-var wav_path = env.WAV_PATH;
-if (!wav_path) {
-  wav_path = "/data/sound_app/";
-}
-var db_name = env.DB_PATH;
-if (!db_name) {
-  db_name = "/data/sound_app/sound_app.db";
-}
-var label_file = env.LABEL_FILE;
-if (!label_file) {
-  label_file = "/data/sound_app/labels.txt";
-}
-var master_node = env.MASTER_NODE;
-if (!master_node) {
-  master_node = "unknown";
-}
+let wav_path = env.WAV_PATH || "/data/sound_app/";
+let db_name = env.DB_PATH || "/data/sound_app/sound_app.db";
+var label_file = env.LABEL_FILE || "/data/sound_app/labels.txt";
+var master_node = env.MASTER_NODE || "unknown";
 
-var minio_access_key = env.MINIO_ACCESS_KEY;
-var minio_secret_key = env.MINIO_SECRET_KEY;
-var uuid = env.RESIN_DEVICE_UUID;
-var short_uuid = uuid.substring(0, 8);
-var menu = [ short_uuid, '#', 'Master', 'https://' + master_node + '.balena-devices.com' ];
-var menu_items = env.MENU_ITEMS;
+let minio_access_key = env.MINIO_ACCESS_KEY;
+let minio_secret_key = env.MINIO_SECRET_KEY;
+let uuid = env.RESIN_DEVICE_UUID;
+let short_uuid = uuid.substring(0, 8);
+let menu = [ short_uuid, '#', 'Master', `https://${master_node}.balena-devices.com` ];
+let menu_items = env.MENU_ITEMS;
 if (menu_items) {
   menu = JSON.parse("[" + string.split() + "]");
 }
-var ready_rows = 0;
-
-var Minio = require('minio')
-var upload_enabled = "OK";
+let ready_rows = 0;
+let upload_enabled = "OK";
 
 if (!minio_access_key || !minio_secret_key) {
   upload_enabled = "No Minio credentials set";
@@ -61,7 +56,7 @@ app.use(express.static('styles'));
 app.use("/public", express.static(wav_path));
 
 // For processing forms
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 // Read in label file
 try {
@@ -71,117 +66,85 @@ try {
   console.log("Error reading label file: ", error);
   upload_enabled = "No label file";
 }
-function getReadyCount(uid, callback){
+
+async function getReadyCount () {
   var query = "SELECT filename FROM wav_file WHERE current_status = 'ready'";
-  db.all(query, function (err, rows) {
-    if(err){
-        console.log(err);
-    }else{
-        callback(rows.length);
-    }
-  });
+  let rows = await dbAll(query);
+  return rows.length
 }
 
-function cb_readyCount(rowcount) {
-  //console.log("print:",rowcount);
-  ready_rows = rowcount;
-}
-
-
-async function doUpload() {
-  return new Promise((resolve, reject) => {
-    let row_id = 0;
-    let sql = "";
-    let frmErr = "NA";
-    let filename = "";
-    let bucket = uuid.substring(0, 7);
-    let metaData = "";
-    if (master_node != "unknown") {
-      sql = "SELECT rowid, filename, user_class, user_description FROM wav_file WHERE current_status = 'ready'";
-      db.all(sql, [], (err,rows) => {
-       if (err) {
-         frmErr = frmErr + err.message;
-         console.error(err.message);
-       }
-       for (const row of rows) {
-         doUploadTasks(row)
-         console.log("Completed one doupload2");
-       }
-       if (frmErr != "NA") {
-         frmErr = "<h4 style='color:red;'>One or more errors during file upload: " + frmErr + "</h4>";
-       } else {
-         frmErr = "<h4 style='color:green;'>File(s) successfully uploaded.</h4>";
-       }
-       resolve(frmErr);
-      });  // end outer db;
-
-    } else {
-      // master node not set
-      frmErr = "<h4 style='color:red;'>Master node unknown. You must set value in balena dashboard.</h4>";
-      reject(frmErr);
-    }
-  });  // end promise
-}
-
-
-async function doUploadTasks(row) {
-
-  let p = new Promise(async (resolve, reject) => {
-    let sql = "";
-    let frmErr = "NA";
-    let bucket = uuid.substring(0, 7);
-
-    // upload to master
-    let filename = row.filename;
-    let row_id = row.rowid;
-    let metaData = {
-      'Content-Type': 'application/octet-stream',
-      'x-amz-meta-rowid': row_id,
-      'x-amz-meta-class': row.user_class,
-      'x-amz-meta-descrip': row.user_description
-    }
-    console.log("uploading: ", filename);
+async function doUpload () {
+  let sql = "";
+  let frmErr = "NA";
+  if (master_node != "unknown") {
     try {
-        url = await minioClient.fPutObject(bucket, filename, wav_path + filename, metaData);
-    } catch(error) {
-        console.log('minio upload catch');
-        resolve(10);
-    }
-    console.log('File ' + filename + ' uploaded successfully.');
-    resolve(10);
-  });
-
-  p.then((result) => {
-    console.log(result);
-    return new Promise((resolve, reject) => {
-      row_id = row.rowid;
-      sql = "UPDATE wav_file SET timestamp_deleted = datetime('now'), timestamp_uploaded = datetime('now'), current_status = 'uploaded' WHERE (rowid = " + row_id + ")";
-      console.log("post upload SQL: ", sql);
-      db.run(sql, err => {
-        if (err) {
-          frmErr = frmErr + " " + err.message;
-        }
-      });
-      resolve(20);
-      });
-  }).then((result) => {
-    console.log(result);
-    return new Promise((resolve, reject) => {
-      console.log("deleting file ", row.filename);
-      fs.unlinkSync(wav_path + row.filename, function (err) {
-      if (err) {
-        frmErr = frmErr + " " + err.message;
+      sql = "SELECT rowid, filename, user_class, user_description FROM wav_file WHERE current_status = 'ready'";
+      let rows = await dbAll(query)
+      for (const row of rows) {
+        await doUploadTasks(row)
+        console.log("Completed one doupload2");
       }
-    });
-    resolve(30);
-    });
-  }).then(result => console.log(result));
+    } catch (error) {
+      frmErr = frmErr + err.message;
+      console.error(err.message);
+    }
+    if (frmErr != "NA") {
+      frmErr = "<h4 style='color:red;'>One or more errors during file upload: " + frmErr + "</h4>";
+    } else {
+      frmErr = "<h4 style='color:green;'>File(s) successfully uploaded.</h4>";
+    }
+  } else {
+    // master node not set
+    frmErr = "<h4 style='color:red;'>Master node unknown. You must set value in balena dashboard.</h4>";
+  }
+}
 
+
+async function doUploadTasks (row) {
+
+  let bucket = uuid.substring(0, 7);
+
+  // upload to master
+  let filename = row.filename;
+  let row_id = row.rowid;
+  let metaData = {
+    'Content-Type': 'application/octet-stream',
+    'x-amz-meta-rowid': row_id,
+    'x-amz-meta-class': row.user_class,
+    'x-amz-meta-descrip': row.user_description
+  }
+
+  // Upload
+  console.log("uploading: ", filename);
+  try {
+    url = await minioClient.fPutObject(bucket, filename, wav_path + filename, metaData);
+  } catch (error) {
+    console.log('minio upload catch');
+  }
+  console.log('File ' + filename + ' uploaded successfully.');
+
+  // Update db
+  row_id = row.rowid;
+  sql = "UPDATE wav_file SET timestamp_deleted = datetime('now'), timestamp_uploaded = datetime('now'), current_status = 'uploaded' WHERE (rowid = " + row_id + ")";
+  console.log("post upload SQL: ", sql);
+  try {
+    await dbRun(sql)
+  } catch (err) {
+    frmErr = frmErr + " " + err.message;
+  }
+
+  // Delete file
+  console.log("deleting file ", row.filename);
+  fs.unlinkSync(wav_path + row.filename, function (err) {
+    if (err) {
+      frmErr = frmErr + " " + err.message;
+    }
+  });
 }
 
 
 
-function getSQL(filter, srtid) {
+function getSQL (filter, srtid) {
 
   var sql = "SELECT rowid, timestamp_created, interpreter_class, interpreter_class2, interpreter_certainty, interpreter_certainty2, current_status, filename, threshold FROM wav_file";
 
@@ -215,20 +178,15 @@ function getSQL(filter, srtid) {
 }
 
 // reply to home page request
-app.get('/', function (req, res) {
-  getReadyCount(0, cb_readyCount);
-  console.log("GETSQL for home page render: ",  getSQL(req.query.filter, req.query.srtid));
-  db.all(getSQL(req.query.filter, req.query.srtid), [], (err,rows) => {
-    if (err) {
-      return console.error(err.message);
-    }
+app.get('/', async function (req, res) {
+  ready_rows = getReadyCount();
+  console.log("GETSQL for home page render: ", getSQL(req.query.filter, req.query.srtid));
+  let rows = await dbAll(getSQL(req.query.filter, req.query.srtid), [])
   res.render('index', { model: rows, srtid: req.query.srtid, fil: req.query.filter, frmErr: 'NA', labels: labels, readyCount: ready_rows, rm: "false", upload_enabled: upload_enabled, menuItems: menu });
-  });
 });
 
-app.post('/', async (req, res, next) => {
+app.post('/', async (req, res) => {
   let frmErr = "NA";
-  let filename = "";
   console.log('Form submitted: ', req.body);
   let sql = "UPDATE wav_file SET ";
   // Validate form input
@@ -237,59 +195,51 @@ app.post('/', async (req, res, next) => {
     if (req.body.predictClass == "other" && (req.body.txtDescription === undefined || req.body.txtDescription == "")) {
       frmErr = "<h4 style='color:red;'>Description is required. File not uploaded!</h4>";
     } else {
-        // upload details form posted
-        // update db here
-        const heard = [req.body.predictClass, req.body.txtDescription, req.body.txtNotes, req.body.hidWavID1];
-        sql = sql + "user_class = ?, user_description = ?, user_notes = ?, current_status = 'ready', timestamp_ready = datetime('now') WHERE (rowid = ?)";
-        console.log("Upload SQL: ", sql);
-        db.run(sql, heard, err => {
-          if (err) {
-            frmErr = err.message;
-          }
-        });
-        // upload file
-        if (frmErr != "NA") {
-          frmErr = "<h4 style='color:red;'>Error uploading file: " + frmErr + "</h4>";
-        } else {
-          frmErr = "<h4 style='color:green;'>File successfully uploaded.</h4>";
-        }
+      // upload details form posted
+      // update db here
+      const heard = [ req.body.predictClass, req.body.txtDescription, req.body.txtNotes, req.body.hidWavID1 ];
+      sql = sql + "user_class = ?, user_description = ?, user_notes = ?, current_status = 'ready', timestamp_ready = datetime('now') WHERE (rowid = ?)";
+      console.log("Upload SQL: ", sql);
+      try {
+        await dbRun(sql, heard)
+      } catch (err) {
+        frmErr = err.message;
+      }
+      // upload file
+      if (frmErr != "NA") {
+        frmErr = "<h4 style='color:red;'>Error uploading file: " + frmErr + "</h4>";
+      } else {
+        frmErr = "<h4 style='color:green;'>File successfully uploaded.</h4>";
+      }
     }  // end else blank description
 
   } else {
-      if (req.body.hidFormName == "id02") {
-        // delete file form posted
-        // update db
-        sql = sql + "timestamp_deleted = datetime('now'), current_status = 'deleted' WHERE (rowid = " + req.body.hidWavID2 + ")";
-        console.log("Delete SQL: ", sql);
-        db.run(sql, err => {
-          if (err) {
-            frmErr = err.message;
-          }
-        });
-        // delete file
-        fs.unlinkSync(wav_path + req.body.hidWavFile, function (err) {
-          if (err) {
-            frmErr = frmErr + " " + err.message;
-          }
-        });
-        if (frmErr != "NA") {
-          frmErr = "<h4 style='color:red;'>Error deleting file: " + frmErr + "</h4>";
-        } else {
-          frmErr = "<h4 style='color:green;'>File successfully deleted.</h4>";
-        }
-      }  else {
-        // Upload form posted
-        await doUpload()
-        //try {
-          //frmErr = await doUpload().catch(error => {
-            //console.log("Await err:", error);
-            //throw error;
-          //});
-        //} catch (error) {
-          //console.log("caught await error");
-        //}
-        console.log("moving on...");
+    if (req.body.hidFormName == "id02") {
+      // delete file form posted
+      // update db
+      sql = sql + "timestamp_deleted = datetime('now'), current_status = 'deleted' WHERE (rowid = " + req.body.hidWavID2 + ")";
+      console.log("Delete SQL: ", sql);
+      try {
+        await dbRun(sql)
+      } catch (err) {
+        frmErr = err.message;
       }
+      // delete file
+      fs.unlinkSync(wav_path + req.body.hidWavFile, function (err) {
+        if (err) {
+          frmErr = frmErr + " " + err.message;
+        }
+      });
+      if (frmErr != "NA") {
+        frmErr = "<h4 style='color:red;'>Error deleting file: " + frmErr + "</h4>";
+      } else {
+        frmErr = "<h4 style='color:green;'>File successfully deleted.</h4>";
+      }
+    } else {
+      // Upload form posted
+      await doUpload()
+      console.log("moving on...");
+    }
   }
 
   res.redirect(req.get('referer'));
