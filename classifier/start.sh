d26afe6bd01660b3b15ecf11dd6c05f474e78a6d
Alan Boris <39385685+alanb128@users.noreply.github.com>
2020-09-14 00:04:02 -0400

    Overwrite model, labels on start

--- classifier/start.sh
+++ classifier/start.sh
@@ -2,11 +2,9 @@
 
 sleep 2
 
-# Copy model and labels to shared volume if if doesn't exist already (-n)
-cp -n /usr/src/app/qmodel3a8p2_edgetpu.tflite /data/sound_app/qmodel3a8p2_edgetpu.tflite
-cp -n /usr/src/app/labels.txt /data/sound_app/labels.txt
-rm /usr/src/app/labels.txt
-rm /usr/src/app/qmodel3a8p2_edgetpu.tflite
+# Copy model and labels to shared volume - overwrites existing
+cp /usr/src/app/qmodel3a8p2_edgetpu.tflite /data/sound_app/qmodel3a8p2_edgetpu.tflite
+cp /usr/src/app/labels.txt /data/sound_app/labels.txt
 
 # Start the classifier
 python3 classifier.py
