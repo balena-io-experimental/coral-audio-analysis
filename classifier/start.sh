32fd1c68d47c7c2d6069dbfe41a8978f1891ceab
Alan Boris <39385685+alanb128@users.noreply.github.com>
2020-09-16 10:52:07 -0400

    Update start.sh

--- classifier/start.sh
+++ classifier/start.sh
@@ -3,7 +3,7 @@
 sleep 2
 
 # Copy model and labels to shared volume - overwrites existing
-cp /usr/src/app/sound_edgetpu.tflite /data/sound_app/qmodel3a8p2_edgetpu.tflite
+cp /usr/src/app/sound_edgetpu.tflite /data/sound_app/sound_edgetpu.tflite
 cp /usr/src/app/labels.txt /data/sound_app/labels.txt
 
 # Start the classifier
